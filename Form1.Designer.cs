b76a6f403297998016405bf47b56027178f9633a
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2021-01-05 23:15:24 -0600

    Form base

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BienvenidoACO
 {
-    partial class Form1
+    partial class FormDoor
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,23 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDoor));
+            this.labelMessage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.BackColor = System.Drawing.Color.Transparent;
+            this.labelMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.labelMessage.Location = new System.Drawing.Point(12, 501);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(0, 17);
+            this.labelMessage.TabIndex = 0;
+            // 
+            // FormDoor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(822, 547);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(822, 535);
+            this.Controls.Add(this.labelMessage);
+            this.Name = "FormDoor";
+            this.Text = "Puerta ACO";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormDoor_FormClosed);
+            this.Load += new System.EventHandler(this.FormDoor_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelMessage;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO.Ports;
+using System.Threading;
 
 namespace BienvenidoACO
 {
-    public partial class Form1 : Form
+    public partial class FormDoor : Form
     {
-        public Form1()
+        System.IO.Ports.SerialPort Port;
+        bool cerrado = false;
+
+        SpeechSynthesizer boca = new SpeechSynthesizer();
+
+
+        public FormDoor()
         {
             InitializeComponent();
+            Port = new System.IO.Ports.SerialPort();
+
+            Port.PortName = "COM3";
+            Port.BaudRate = 9600;
+            Port.ReadTimeout = 500;
+            try
+            {
+                Port.Open();
+            }catch
+            {
+
+            }
+            
+            
+        }
+
+        private void FormDoor_Load(object sender, EventArgs e)
+        {
+
+            Thread Hilo = new Thread(EscucharHilo);
+            Hilo.Start();
+            
+        }
+
+        private void EscucharHilo()
+        {
+            while (!cerrado)
+            {
+                try
+                {
+                    string acceso = Port.ReadLine();
+                    labelMessage.Invoke(new MethodInvoker(
+                        delegate
+                        {
+                            labelMessage.Text = acceso;
+                            boca.SpeakAsync(acceso);
+                        }
+
+                        ));
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        private void FormDoor_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            cerrado = true;
+            if (Port.IsOpen)
+                Port.Close();
         }
     }
 }
